e9afd425919aa683528629541465ee5c430cb2e2
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2024-10-19 04:09:41 +0200

    test

---
+++ Logger/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using Discord;
+
+namespace DiscordKor.Log;
+
+public interface ILogger
+{
+    // Establish required method for all Loggers to implement
+    public Task Log(LogMessage message);
+}
